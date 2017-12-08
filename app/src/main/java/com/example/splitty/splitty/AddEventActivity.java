ef20a8042bb65d3d07c8cfd3e6b1a478f1c0aa37
SOMEONEPC\luxor <luxor37@gmail.com>
2017-12-08 15:47:15 -0500

    Fix support.v7.widget.toolbar from mainactivity and fixed a null pointer exception

--- app/src/main/java/com/example/splitty/splitty/AddEventActivity.java
+++ app/src/main/java/com/example/splitty/splitty/AddEventActivity.java
@@ -1,14 +1,13 @@
 package com.example.splitty.splitty;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.support.v7.widget.Toolbar;
+        import android.view.View;
 
 public class AddEventActivity extends AppCompatActivity {
-
+    private Intent mainIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,8 +15,11 @@ public class AddEventActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-
+        mainIntent = new Intent(this, MainActivity.class);
     }
 
+    public void createEvent (View v){
+
+        startActivity(mainIntent);
+    }
 }
