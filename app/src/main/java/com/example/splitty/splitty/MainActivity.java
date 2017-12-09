d12cc79024ffa639eeb1d1cd7ee5d9bbebedb4d9
wfour <wfournier2710@gmail.com>
2017-12-08 20:51:30 -0500

    added scroll view with all events in main activity

--- app/src/main/java/com/example/splitty/splitty/MainActivity.java
+++ app/src/main/java/com/example/splitty/splitty/MainActivity.java
@@ -3,13 +3,25 @@ package com.example.splitty.splitty;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.support.v7.widget.Toolbar;
+import android.widget.ScrollView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import com.example.splitty.splitty.Classes.Event;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private Intent addEventIntent;
+    private DatabaseManager db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +30,10 @@ public class MainActivity extends AppCompatActivity {
         Toolbar tb = findViewById(R.id.toolbar);
         setSupportActionBar(tb);
 
+        db = new DatabaseManager(this);
+
         addEventIntent = new Intent(this, AddEventActivity.class);
+        populate();
     }
 
     @Override
@@ -48,6 +63,34 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void populate(){
+        ArrayList<Event> events = db.selectAllEvents();
+
+        for(int i = 0; i < 50; i++){
+            Event event = new Event();
+            event.setName("Test" +i);
+            events.add(event);
+        }
+
+        TableLayout event_table = new TableLayout(this);
+
+        for(Event e : events){
+            TableRow tr = new TableRow(this);
+            tr.setGravity(Gravity.CENTER);
+
+            TextView tv = new TextView(this);
+            String text = e.getName();
+            tv.setText(text);
+            tv.setTextSize(40);
+
+            tr.addView(tv);
+            event_table.addView(tr);
+        }
+
+        ScrollView event_scroll = findViewById(R.id.event_scroll);
+        event_scroll.addView(event_table);
     }
 
     public void addEvent(View v){
